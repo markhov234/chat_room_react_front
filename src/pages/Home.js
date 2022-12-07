8c05a4e81cce88fa255b69f668e2b130b4e97521
Marc-Antoine Hovington <hovington234@gmail.com>
2022-12-07 16:06:16 -0500

    Added Home elements from bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import {Row,Col, Button} from "react-bootstrap"
+import {LinkContainer} from "react-router-bootstrap"
 
 function Home() {
   return (
-    <div>
-      HOME
-    </div>
+    <Row>
+      <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+        <div>
+          <h1>Share the world with your friends</h1>
+          <p>
+            Chat App lets you connect with the world
+          </p>
+          <LinkContainer to="/chat">
+            <Button variant="success">
+              Get Started
+            <i className='fas fa-comments home-message-icon'></i>
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+    </Row>
   )
 }
 
